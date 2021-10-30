88ab33e8046f593263516d91c840305309c34c1b
Walther <wsfrancootero@gmail.com>
2021-10-29 20:32:59 -0500

    no divisible by 3

--- retos.py
+++ retos.py
@@ -1,5 +1,7 @@
 #create a list of numbers ^2
-
-numbers = [x**2 for x in range(1,100)]
-
+numbers = [x**2 for x in range(1,101)]
 print(numbers)
+
+#no divisible by 3
+squares = [x  for x in range(1,101) if x %3!=0]
+print(squares)
